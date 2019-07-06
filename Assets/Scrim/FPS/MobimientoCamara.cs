3ff8147d670b80cf9f9ea4b883b4ea1bd07295fa
Kuuragos <alejandrab.19401@gmail.com>
2019-07-06 12:55:10 -0500

    First commit

---
+++ Assets/Scrim/FPS/MobimientoCamara.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobimientoCamara : MonoBehaviour
+{
+    float mauseX;
+    float mauseY;
+    public bool InvertedMouse;
+    
+    void Update()
+    {
+        //Aqui obtenemos la posicion del Mouse tanto en el eje Y como en el X y la vinculamos a la camara 
+        Vector3 mousePosition = Input.mousePosition;
+        mauseX += Input.GetAxis("Mouse X");
+        if (InvertedMouse)
+        {
+            mauseY += Input.GetAxis("Mouse Y");
+        }else
+        {
+            mauseY -= Input.GetAxis("Mouse Y");
+        }
+        // aqui se ajusta la camara de forma manual
+        Debug.Log(mauseX);
+        transform.eulerAngles = new Vector3(mauseY, mauseX, 0);
+    }
+}
---
+++ Assets/Scrim/FPS/MoverPersonaje.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverPersonaje : MonoBehaviour
+{
+    float eje_X;
+    public float speed = 0.1f;
+    void Update()
+    {
+        /* Aqui establesemos en el personaje rporacion en el eje x para que
+         * al momento de girar la camara tambien gira el personaje para 
+         siguir hacia la pocicion del maus en el mismo eje x*/
+        eje_X += Input.GetAxis("Mouse X");
+        transform.eulerAngles = new Vector3(0, eje_X, 0);
+
+        //Aqui asignamos el movimiento del personaje a las teclas WASD 
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed;
+        }
+    }
+}
